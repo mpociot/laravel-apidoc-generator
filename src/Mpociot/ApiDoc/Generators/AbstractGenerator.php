a94d9d4cb93bca06707a833c2b3384e895d0eb56
Oyebanji Jacob Mayowa <oyebanji.jacob@andela.com>
2016-06-09 23:52:15 +0100

    Fix StyleCI issue

--- src/Mpociot/ApiDoc/Generators/AbstractGenerator.php
+++ src/Mpociot/ApiDoc/Generators/AbstractGenerator.php
@@ -143,6 +143,7 @@ abstract class AbstractGenerator
             if ($parameterReflection instanceof FormRequest && method_exists($parameterReflection, 'validator')) {
                 return $parameterReflection->validator()->getRules();
             } 
+            
             return $parameterReflection->rules();
         }
 
