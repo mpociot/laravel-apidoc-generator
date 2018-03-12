460d221a075f7dd0ab5dfb913753b1c049dbb558
sempixel <sempixel@users.noreply.github.com>
2018-03-12 17:28:17 +0100

    Fix test

--- tests/ApiDocGeneratorTest.php
+++ tests/ApiDocGeneratorTest.php
@@ -345,7 +345,7 @@ class ApiDocGeneratorTest extends TestCase
         $this->assertTrue(is_array($parsed));
         $this->assertArrayHasKey('showresponse', $parsed);
         $this->assertTrue($parsed['showresponse']);
-        $this->assertSame($parsed['response'], '"{\n data: [],\n}"');
+        $this->assertSame($parsed['response'], "{\n data: [],\n}");
     }
 
     public function testCanParseTransformerTag()
