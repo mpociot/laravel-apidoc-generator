02fb719d0d6c25e6ce72f30dc8b9604449061156
shalvah <diakon.ng@gmail.com>
2019-09-12 23:21:50 +0100

    Update python template to work with new clean*Params format (fixes #587)

--- resources/views/partials/example-requests/python.blade.php
+++ resources/views/partials/example-requests/python.blade.php
@@ -5,22 +5,28 @@ import json
 url = '{{ rtrim($baseUrl, '/') }}/{{ ltrim($route['boundUri'], '/') }}'
 @if(count($route['cleanBodyParameters']))
 payload = {
-	@foreach($route['cleanBodyParameters'] as $attribute => $parameter)
-	    '{{ $attribute }}': '{{ $parameter['value'] }}'@if(!($loop->last)),@endif  {{ !$parameter['required'] ? '# optional' : '' }}
-	@endforeach
+@foreach($route['cleanBodyParameters'] as $parameter => $value)
+    '{{ $parameter }}': '{{ $value }}'@if(!($loop->last)),
+@endif
+@endforeach
+
 }
 @endif
 @if(count($route['cleanQueryParameters']))
 params = {
-	@foreach($route['cleanQueryParameters'] as $attribute => $parameter)
-	    '{{ $attribute }}': '{{ $parameter['value'] }}'@if(!($loop->last)),@endif  {{ !$parameter['required'] ? '# optional' : '' }}
-	@endforeach
+@foreach($route['cleanQueryParameters'] as $parameter => $value)
+	'{{ $parameter }}': '{{ $value }}'@if(!($loop->last)),
+@endif
+@endforeach
+
 }
 @endif
 headers = {
-	@foreach($route['headers'] as $header => $value)
-	    '{{$header}}': '{{$value}}'@if(!($loop->last)),@endif
-	@endforeach
+@foreach($route['headers'] as $header => $value)
+	'{{$header}}': '{{$value}}'@if(!($loop->last)),
+@endif
+@endforeach
+
 }
 response = requests.request('{{$route['methods'][0]}}', url, headers=headers{{ count($route['cleanBodyParameters']) ? ', json=payload' : '' }}{{ count($route['cleanQueryParameters']) ? ', params=params' : ''}})
 response.json()
